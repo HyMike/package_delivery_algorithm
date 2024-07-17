631fd81e15b2bc8b0f8b761ede6afc6725b6f0e1
Michael Hy <hymike88@gmail.com>
2024-07-17 13:32:21 -0700

    made switches to function in trucks and main.py

--- main.py
+++ main.py
@@ -25,6 +25,45 @@ address_list, distance_list = load_csv_files()
 # Initialize the hash table
 packages_table = CreateHashMap()
 
+
+def addresss(address, AddressCSV):
+    for row in AddressCSV:
+        if address in row[2]:
+            return int(row[0])
+
+def Betweenst(addy1, addy2, DistanceCSV):
+    distance = DistanceCSV[addy1][addy2]
+    if distance == '':
+        distance = DistanceCSV[addy2][addy1]
+    return float(distance)
+
+def truckDeliverPackages(truck, packageHash, AddressCSV, DistanceCSV):
+    enroute = []
+    for packageID in truck.packages:
+        package = packageHash.search(packageID)
+        enroute.append(package)
+
+    truck.packages.clear()
+
+    while len(enroute) > 0:
+        nextAddy = 2000
+        nextPackage = None
+        for package in enroute:
+            if package.ID in [25, 6]:
+                nextPackage = package
+                nextAddy = Betweenst(addresss(truck.currentLocation, AddressCSV), addresss(package.street, AddressCSV), DistanceCSV)
+                break
+            if Betweenst(addresss(truck.currentLocation, AddressCSV), addresss(package.street, AddressCSV), DistanceCSV) <= nextAddy:
+                nextAddy = Betweenst(addresss(truck.currentLocation, AddressCSV), addresss(package.street, AddressCSV), DistanceCSV)
+                nextPackage = package
+        truck.packages.append(nextPackage.ID)
+        enroute.remove(nextPackage)
+        truck.miles += nextAddy
+        truck.currentLocation = nextPackage.street
+        truck.time += datetime.timedelta(hours=nextAddy / 18)
+        nextPackage.deliveryTime = truck.time
+        nextPackage.departureTime = truck.departTime
+
 # Load package data into the hash table
 loading_package_data('CSV/WGUPS_Package.csv', packages_table)
 
---
+++ trucks.py
@@ -0,0 +1,13 @@
+import datetime
+
+class Trucks:
+    def __init__(self, speed, miles, current_location, departure_time, packages):
+        self.speed = speed
+        self.miles = miles
+        self.currentLocation = current_location
+        self.time = departure_time
+        self.departure_time = departure_time
+        self.packages = packages
+
+    def __str__(self):
+        return "%s,%s,%s,%s,%s,%s" % (self.speed, self.miles, self.current_location, self.time, self.departure_time, self.packages)
