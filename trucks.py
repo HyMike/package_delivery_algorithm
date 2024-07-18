cc72bf50f6940783a876a38e3bed4fd18f303f49
Michael Hy <hymike88@gmail.com>
2024-07-17 20:47:49 -0700

    deleted uncessary code in Trucks class

--- trucks.py
+++ trucks.py
@@ -1,18 +1,3 @@
-# import datetime
-#
-# #Create a Truck Object that allows to track it's location, time and packages.
-# class Trucks:
-#     def __init__(self, speed, miles, current_location, departure_time, packages):
-#         self.speed = speed
-#         self.miles = miles
-#         self.current_location = current_location
-#         self.time = departure_time
-#         self.departure_time = departure_time
-#         self.packages = packages
-#
-#     def __str__(self):
-#         return "%s,%s,%s,%s,%s,%s" % (self.speed, self.miles, self.current_location, self.time, self.departure_time, self.packages)
-
 import datetime
 
 # Create a Truck Object that allows tracking its location, time, and packages.
